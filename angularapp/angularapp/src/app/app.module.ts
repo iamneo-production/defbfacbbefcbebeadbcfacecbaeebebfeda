e830195a802962d2605c3a09c7473e636a9d1940
Iamneo <techadmin@iamneo.ai>
2023-08-05 07:46:45 +0000

    Backup Commit

--- angularapp/angularapp/src/app/app.module.ts
+++ angularapp/angularapp/src/app/app.module.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppComponent {
-  title = 'Calory Tracker';
-}
+export class AppModule { }
